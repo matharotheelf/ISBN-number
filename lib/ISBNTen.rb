bcc80db1620465f529262982b26c414e9f945ac9
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-05-15 17:56:34 +0100

    Refactor: extracted from valid? to correct dahses? method

--- lib/ISBNTen.rb
+++ lib/ISBNTen.rb
@@ -6,6 +6,12 @@ class ISBNTen
   end
 
   def valid?
-    @number.is_a?(String) && @number.length == 13 && @number[1] == '-' && @number[5] == '-' && @number[11] == '-'
+    @number.is_a?(String) && @number.length == 13 && correct_dashes?
+  end
+
+  private
+
+  def correct_dashes?
+    @number[1] == '-' && @number[5] == '-' && @number[11] == '-'
   end
 end
