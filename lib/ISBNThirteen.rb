4c978954d28857f438654c378cf47c491522f68e
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-05-17 14:51:09 +0100

    Refactor: extracted correct dashes from valid?

--- lib/ISBNThirteen.rb
+++ lib/ISBNThirteen.rb
@@ -6,6 +6,12 @@ class ISBNThirteen
   end
 
   def valid?
-    @number.is_a?(String) && @number.length == 17 && @number[3] == '-' && @number[5] == '-' && @number[9] == '-' && @number[15] == '-'
+    @number.is_a?(String) && @number.length == 17 && correct_dashes?
+  end
+
+  private
+
+  def correct_dashes?
+    @number[3] == '-' && @number[5] == '-' && @number[9] == '-' && @number[15] == '-'
   end
 end
