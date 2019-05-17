1866bec6e68700088be5f3f86c6eff0570f96110
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-05-17 14:52:29 +0100

    Refactor: extracted sring length seventeen from valid?

--- lib/ISBNThirteen.rb
+++ lib/ISBNThirteen.rb
@@ -6,12 +6,16 @@ class ISBNThirteen
   end
 
   def valid?
-    @number.is_a?(String) && @number.length == 17 && correct_dashes?
+    string_length_seventeen? && correct_dashes?
   end
 
   private
 
   def correct_dashes?
     @number[3] == '-' && @number[5] == '-' && @number[9] == '-' && @number[15] == '-'
+  end
+
+  def string_length_seventeen?
+    @number.is_a?(String) && @number.length == 17
   end
 end
